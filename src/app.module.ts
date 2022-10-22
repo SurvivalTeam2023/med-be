e3747ffb126ac09f8ce868a3558bda825229efdb
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-10-23 00:53:07 +0700

    [MED-1] Init Project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmConfigModule } from './common/typeorm/typeorm.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // imports: [TypeOrmConfigModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appConfig } from './common/config/app.config';
+import { swaggerConfig } from './common/config/swagger.config';
+import { SERVER_PORT } from './environments';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await appConfig(app);
+  await swaggerConfig(app);
+  await app.listen(SERVER_PORT, () => {
+    console.log(`Application is running at port: ${SERVER_PORT}`);
+  });
 }
 bootstrap();
