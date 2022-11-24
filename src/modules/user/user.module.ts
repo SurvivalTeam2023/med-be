25cabc3b6a91e8a78c16f8a56a01bd88875bcc1d
BanhsBao <huynhbaofaker@gmail.com>
2022-11-25 01:20:52 +0700

    MED-34 Resolve conflict

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FilesService } from 'src/modules/files/files.service';
 import User from './entities/user.entity';
 import { UserController } from './user.controller';
 import { KeycloakModule } from '../../common/config/keycloak.config';
 import { File } from '../files/entities/file.entity';
 import { HttpModule } from '@nestjs/axios';
+import { UserService } from './user.services';
 @Module({
   imports: [TypeOrmModule.forFeature([User, File]), KeycloakModule, HttpModule],
   controllers: [UserController],
-  providers: [FilesService],
+  providers: [UserService],
 })
 export class UserModule {}
