9f6e3218964208ebb39068573ffa9971ee22c9ce
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-12-12 21:49:47 +0700

    demo CICD (#31)

--- src/common/config/swagger.config.ts
+++ src/common/config/swagger.config.ts
@@ -5,7 +5,7 @@ import { SWAGGER_URL } from 'src/environments';
 export const swaggerConfig = async (app: INestApplication) => {
   const option = new DocumentBuilder()
     .setTitle('MED Capstone')
-    .setDescription('Meditation healing your soul')
+    .setDescription('Meditation healing your soul Demo CICD')
     .setVersion('0.1')
     .addBearerAuth();
   const document = SwaggerModule.createDocument(app, option.build());
