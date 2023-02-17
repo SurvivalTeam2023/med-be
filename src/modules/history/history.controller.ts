0badca0c0741cdf73cc9924ab83796af8d5ce737
Nghia Nguyen <82258622+ntbnghia2308@users.noreply.github.com>
2023-02-17 14:32:37 +0700

    add get history API (#74)

--- src/modules/history/history.controller.ts
+++ src/modules/history/history.controller.ts
@@ -1,22 +1,34 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
-import { Unprotected } from "nest-keycloak-connect";
-import { RequestPayload } from "src/decorator/requestPayload.decorator";
-import CreateHistoryDTO from "./dto/createHistory.dto";
-import { HistoryEntity } from "./entities/history.entity";
-import HistoryService from "./history.service";
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Unprotected } from 'nest-keycloak-connect';
+import { RequestPayload } from 'src/decorator/requestPayload.decorator';
+import CreateHistoryDTO from './dto/createHistory.dto';
+import { HistoryEntity } from './entities/history.entity';
+import HistoryService from './history.service';
 
 @ApiTags('History')
 @Controller('history')
 @ApiBearerAuth()
 @Controller('rest/history')
-
-export default class HistoryController { 
-    constructor(private readonly historyService: HistoryService) {}
+export default class HistoryController {
+  constructor(private readonly historyService: HistoryService) {}
   @ApiOperation({ summary: 'create History' })
   @Unprotected()
-  @Post("create")
-  async createHistory(@Body() dto: CreateHistoryDTO, @RequestPayload() token: string): Promise<HistoryEntity>  {
+  @Post('create')
+  async createHistory(
+    @Body() dto: CreateHistoryDTO,
+    @RequestPayload() token: string,
+  ): Promise<HistoryEntity> {
     return this.historyService.createHistory(dto, token);
+  }
+
+  @ApiOperation({ summary: 'find Favorite by userId' })
+  @Get(':userId')
+  @Unprotected()
+  async getAllFavorite(
+    @Param('userId') userId: string,
+  ): Promise<HistoryEntity[]> {
+    return this.historyService.findHistory(userId);
   }
 }
