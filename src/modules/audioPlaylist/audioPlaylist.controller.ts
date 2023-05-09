873cfa40a82cc71b663e68f86a8eb526bd4430f6
hnim1922 <71585912+hnim1922@users.noreply.github.com>
2023-05-09 14:56:01 +0700

    add role for function

--- src/modules/audioPlaylist/audioPlaylist.controller.ts
+++ src/modules/audioPlaylist/audioPlaylist.controller.ts
@@ -4,19 +4,20 @@ import AudioPlaylistService from "./audioPlaylist.services";
 import { AudioPlaylistEntity } from "./entities/audioPlaylist.entity";
 import { createAudioPlaylistDTO } from "./dto/createAudioPlaylist.dto";
 import { RequestPayload } from "src/decorator/requestPayload.decorator";
-import { Unprotected } from "nest-keycloak-connect";
+import { Roles } from "nest-keycloak-connect";
+import { USER_CLIENT_ROLE } from "src/common/enums/userClientRole.enum";
 
 @ApiTags('AudioPlaylists')
 @Controller('audioPlaylist')
 @ApiBearerAuth()
 export default class AudioPlaylistController {
     constructor(private readonly audioPlaylistService: AudioPlaylistService) { }
-    @Unprotected()
+    @Roles({ roles: [USER_CLIENT_ROLE.ARTIST, USER_CLIENT_ROLE.USER, USER_CLIENT_ROLE.SUBSCRIBER] })
     @Post()
     async addAudioToPlaylist(@Body() dto: createAudioPlaylistDTO, @RequestPayload() token: string): Promise<AudioPlaylistEntity> {
         return await this.audioPlaylistService.addAudioToPlaylist(dto, token)
     }
-    @Unprotected()
+    @Roles({ roles: [USER_CLIENT_ROLE.ARTIST, USER_CLIENT_ROLE.USER, USER_CLIENT_ROLE.SUBSCRIBER] })
     @Delete()
     async removeAudioFromPlaylist(@Query('audioId') audioId: number, @Query('playlistId') playlistId: number, @RequestPayload() token: string) {
         return await this.audioPlaylistService.removeAudioFromPlaylist(playlistId, audioId, token)
