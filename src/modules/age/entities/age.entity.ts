37f1b51f06007c1417ab0c40a210df31aa73338c
hnim1922 <71585912+hnim1922@users.noreply.github.com>
2023-07-01 14:29:39 +0700

    add Age table

---
+++ src/modules/age/entities/age.entity.ts
@@ -0,0 +1,16 @@
+import { QuestionEntity } from "src/modules/question/entities/question.entity";
+import { Entity, BaseEntity, Column, OneToMany } from "typeorm";
+
+@Entity('age')
+export class AgeEntity extends BaseEntity {
+    @Column({ name: 'start_age' })
+    public startAge: number;
+
+    @Column({ name: 'end_age' })
+    public endAge: number;
+
+    @OneToMany(() => QuestionEntity, (question) => question.age, {
+        cascade: true,
+    })
+    public question: QuestionEntity[];
+}
