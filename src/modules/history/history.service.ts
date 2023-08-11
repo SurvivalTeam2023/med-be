1d7fb48a4f49676e37f5c562eabb016e49eb3de8
hnim1922 <71585912+hnim1922@users.noreply.github.com>
2023-08-11 15:43:15 +0700

    add get top10 listened

--- src/modules/history/history.service.ts
+++ src/modules/history/history.service.ts
@@ -93,4 +93,21 @@ export default class HistoryService {
 
     return history.length;
   }
+
+
+  async getTop10Listened(): Promise<any> {
+    const history = await this.historyRepo
+      .createQueryBuilder('history')
+      .leftJoin('history.audio', 'audio')
+      .select(['audio.name', 'audio.artist', 'SUM(history.count) as sumCount'])
+      .groupBy('history.audio_id')
+      .orderBy("sumCount", "DESC")
+      .limit(10)
+      .getRawMany()
+
+    console.log(history, 'haha');
+
+    return history
+
+  }
 }
