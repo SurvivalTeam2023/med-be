40c7f3b0c8397d5d823dd1051fabfd86d97cfa3e
hnim1922 <71585912+hnim1922@users.noreply.github.com>
2023-09-26 22:15:02 +0700

    update respone

--- src/modules/fmc/fmc.service.ts
+++ src/modules/fmc/fmc.service.ts
@@ -1,20 +1,20 @@
 import * as admin from 'firebase-admin';
 import SendChatDTO from './dto/sendChat.dto';
-import { PRIVATE_KEY } from 'src/environments';
+import { CLIENT_EMAIL, FCM_PROJECT_ID, PRIVATE_KEY } from 'src/environments';
 
 export class FirebaseAdminService {
     private readonly admin: admin.app.App;
     constructor() {
         this.admin = admin.initializeApp({
             credential: admin.credential.cert({
-                projectId: process.env.FCM_PROJECT_ID,
-                clientEmail: process.env.CLIENT_EMAIL,
+                projectId: FCM_PROJECT_ID,
+                clientEmail: CLIENT_EMAIL,
                 privateKey: PRIVATE_KEY
             }),
         });
     }
 
-    async sendNotification(dto: SendChatDTO): Promise<void> {
+    async sendNotification(dto: SendChatDTO): Promise<any> {
         const message = {
             notification: {
                 title: dto.title,
